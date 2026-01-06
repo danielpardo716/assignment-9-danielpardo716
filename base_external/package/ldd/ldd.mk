0800eef7fced3e628d43d3bf6b09a31772b1aced
Daniel Pardo <dpardo25@proton.me>
2026-01-06 15:16:09 -0500

    Completed assignment 7

--- base_external/package/ldd/ldd.mk
+++ base_external/package/ldd/ldd.mk
@@ -10,5 +10,8 @@ LDD_SITE = git@github.com:cu-ecen-aeld/assignment-7-danielpardo716.git
 LDD_SITE_METHOD = git
 LDD_GIT_SUBMODULES = YES
 
+LDD_MODULE_SUBDIRS = misc-modules scull
+LDD_MODULE_MAKE_OPTS = KVERSION=$(LINUX_VERSION_PROBED)
+
 $(eval $(kernel-module))
 $(eval $(generic-package))
